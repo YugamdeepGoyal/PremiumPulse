d754961ac26cb9aa1dd2b32168f85e6526537b3d
YugamdeepGoyal <yugamdeep@gmail.com>
2026-06-27 22:38:24 +0530

    Adding some changes

--- load_data.py
+++ load_data.py
@@ -3,11 +3,12 @@ from sklearn.model_selection import train_test_split
 import os
 import kagglehub
 
-path = kagglehub.dataset_download("mosapabdelghany/medical-insurance-cost-dataset")
-csv_path = os.path.join(path, "insurance.csv")
-df = pd.read_csv(csv_path)
+
 
 def get_dataset():
+    path = kagglehub.dataset_download("mosapabdelghany/medical-insurance-cost-dataset")
+    csv_path = os.path.join(path, "insurance.csv")
+    df = pd.read_csv(csv_path)
     X = df.drop(columns=["charges"])
     y = df["charges"]
     X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42, shuffle=True)

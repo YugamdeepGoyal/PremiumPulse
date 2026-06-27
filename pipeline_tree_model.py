5ef1342c90b0576a4f501bc86c50f0a696a426b3
YugamdeepGoyal <yugamdeep@gmail.com>
2026-06-27 23:16:19 +0530

    Adding some changes

--- pipeline_tree_model.py
+++ pipeline_tree_model.py
@@ -1,20 +1,6 @@
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-from sklearn.preprocessing import LabelEncoder, OrdinalEncoder, FunctionTransformer
+from sklearn.preprocessing import OrdinalEncoder, FunctionTransformer
 from sklearn.pipeline import Pipeline
 from sklearn.compose import ColumnTransformer
-from sklearn.model_selection import train_test_split, GridSearchCV, RandomizedSearchCV
-from sklearn.metrics import (
-    r2_score,
-    mean_absolute_error,
-    mean_squared_error,
-    root_mean_squared_error,
-)
-from sklearn.tree import DecisionTreeRegressor
-from sklearn.tree import plot_tree
-from load_data import get_dataset
 
 pass_through_cols = ["age", "bmi", "children", "smoker_bmi", "smoker_obese"]
 cols_to_encode = ["sex", "smoker", "region"]
